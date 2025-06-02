06cff73c8e0352f72d9051615f1d1d4f1bbb7e62
Goddivor <todjropatrice2002@gmail.com>
2025-06-02 01:51:53 +0000

    update(routes): update routing in main.tsx and page/index.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { AppRouter } from "./pages";
+import { ToastProvider } from "./context/toast-context";
+import { ToastContainer } from "./components/Toast";
 
-createRoot(document.getElementById("root")!).render(<AppRouter />);
+createRoot(document.getElementById("root")!).render(
+  <ToastProvider>
+    <AppRouter />
+    <ToastContainer />
+  </ToastProvider>
+);
