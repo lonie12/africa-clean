d1d930f7c802d2e7820df90567b20f60dba69e9a
Goddivor <todjropatrice2002@gmail.com>
2025-06-02 01:50:17 +0000

    feat(routes): add utils routes file (will be develop later)

---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ContactPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <p>
+                Feel free to reach out to us with any questions or inquiries.
+            </p>
+        </div>
+    );
+};
+
+export default ContactPage;
---
+++ src/pages/quote/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuotePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Quote Page</h1>
+        </div>
+    );
+};
+
+export default QuotePage;
